352b72bb04ccde54effb543ee578448198cfee32
bintadam <zakiyaadan12@gmail.com>
2023-06-17 16:34:01 +0200

    fixed errors

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
+console.log(countries)
 const btnStart = document.querySelector(".btn-start");
 const btnSearch =  document.querySelector('.btn-search');
 const btnSort =  document.querySelector(".btn-sort");
 const hero = document.querySelector(".hero")
 
 const countriesLength = countries.length
-console.log(countriesLength)
-// hero.classList.add("total")
-// document.querySelector(".total").textContent = `Toatal Number of countries: ${countriesLength}`;
+const total = document.createElement("p");
+total.textContent = `Total Number of countries: ${countriesLength}`;
+hero.appendChild(total)
+
+const startingWord = () => {
+    const search = document.querySelector("#search").value
+    console.log(search)
+    const startWord = search
+    const result = countries.filter(country => country.startsWith(startWord))
+    return result
+}
 
 btnStart.addEventListener("click", function(e){
     e.preventDefault()
-    const search = document.querySelector("#search").value
-    console.log(search)
+    btnStart.style.backgroundColor = "rgb(98, 23, 169)"
+    startingWord()
 })
